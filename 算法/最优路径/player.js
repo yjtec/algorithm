4cb0a9329cfc3edb84179880a0994d6b95e9c0f3
阿鑫 <18716463@qq.com>
2021-03-11 17:47:10 +0800

    多点路线备份

--- 算法/最优路径/player.js
+++ 算法/最优路径/player.js
@@ -5,15 +5,8 @@ class player {
         this.w = props.w;//图片最大宽度
         this.h = props.h;//图片最大高度
         this.map = new Array();//地图数据，0代表可行走，正数代表不能行走（墙）
-        this.init();
-    }
-    init() {
-        this.dis = new Array();//已经检测过的点距离起始点的距离数据
-        this.start = new Array();//起始点
-        this.end = new Array();//结束点
-        this.open = new Array();//所有待检测的点
-        this.disLast = new Array();//上一点的坐标
-        this.cnts = 0;//总循环次数
+        this.points = new Array();
+        this.results = new Array();
         if (this.map.length <= 0) {
             this.initMap();
         }
@@ -21,48 +14,66 @@ class player {
     initMap() {
         for (var $i = 0; $i < this.w; $i++) {
             this.map[$i] = new Array();
-            this.dis[$i] = new Array();
-            this.disLast[$i] = new Array();
             for (var $j = 0; $j < this.h; $j++) {//随机生成地图
-                this.map[$i][$j] = parseInt(Math.random() * 5) > 0 ? 0 : 1; //0代表可行走，1代表不可行走（墙）
-                this.dis[$i][$j] = -1; //-1代表还没有检查的点
+                this.map[$i][$j] = parseInt(Math.random() * 10) > 0 ? 0 : 1; //0代表可行走，1代表不可行走（墙）
             }
         }
-        while (true) {
-            this.start = [parseInt(Math.random() * (this.w - 1) + 1), parseInt(Math.random() * (this.h - 1) + 1)];
-            this.end = [parseInt(Math.random() * (this.w - 1) + 1), parseInt(Math.random() * (this.h - 1) + 1)];
-            this.dis[this.start[0]][this.start[1]] = 0;
-            this.disLast[this.start[0]][this.start[1]] = [this.start[0], this.start[1]];
-            if (this.map[this.start[0]][this.start[1]] == 0 && this.map[this.end[0]][this.end[1]] == 0 && this.start[0] != this.end[0] && this.start[1] != this.end[1]) {
-                break;
-            }
+    }
+    randStart2End() {
+        for (var i = 0; i < 2; i++) {
+            let point = [parseInt(Math.random() * (this.w - 1) + 1), parseInt(Math.random() * (this.h - 1) + 1)];
+            this.points.push(point);
+//            this.dis[point[0]][point[1]] = 0;
+//            this.disLast[point[0]][point[1]] = [point[0], point[1]];
+//            if (this.map[point[0]][point[1]] != 0) {
+//                i--;
+//            }
         }
-//        console.log(this.map, this.start, this.end);
     }
     main() {
-        var timestamp = (new Date()).valueOf();
-        var cPoint = this.start;//start点设置为当前点
+        for (var x = 0; x < this.points.length - 1; x++) {
+            console.log(x)
+            let startPoint = this.points[x];
+            let endPoint = this.points[x + 1];
+            let line = this.getRoad(startPoint, endPoint)
+            this.results.push(line);
+        }
+        console.log(this.results)
+        this.drawMap();
+        this.drawRouad();
+        return this.results;
+    }
+    getRoad(startPoint, endPoint) {
+        var cPoint = startPoint;
+        this.init();
         do {
             this.getRound8Point(cPoint);//当前点周围的八个点分别检测，并按照从大到小顺序压入open数组
             var cPoint = this.open.pop();//取得最后一个元素，也就是最小的元素
-            if (cPoint[0] == this.end[0] && cPoint[1] == this.end[1]) {//如果这个元素是end点，也就是寻找到了结束点，即可跳出循环，这里是跟广度最大的区别，因为上一步取得的是最小点，也就保证了这里跳出的时候，是最短路径
+            if (cPoint[0] == endPoint[0] && cPoint[1] == endPoint[1]) {//如果这个元素是end点，也就是寻找到了结束点，即可跳出循环，这里是跟广度最大的区别，因为上一步取得的是最小点，也就保证了这里跳出的时候，是最短路径
                 break;
             }
         } while (cPoint);
-        var timestamp2 = (new Date()).valueOf();
-        console.log("计算路径时长：", (timestamp2 - timestamp) / 1000)
-        console.log('循环次数：', this.cnts);
-        this.drawMap();
-        var timestamp3 = (new Date()).valueOf();
-        console.log("画图时长：", (timestamp3 - timestamp2) / 1000)
-        return this.getResult();
+        return this.getResult(endPoint, startPoint);
+    }
+    init() {
+        this.dis = new Array();//已经检测过的点距离起始点的距离数据
+        this.start = new Array();//起始点
+        this.end = new Array();//结束点
+        this.open = new Array();//所有待检测的点
+        this.disLast = new Array();//上一点的坐标
+        for (var $i = 0; $i < this.w; $i++) {
+            this.dis[$i] = new Array();
+            this.disLast[$i] = new Array();
+            for (var $j = 0; $j < this.h; $j++) {//随机生成地图
+                this.dis[$i][$j] = -1; //-1代表还没有检查的点
+            }
+        }
     }
     getRound8Point(cPoint) {
         var distance = this.dis[cPoint[0]][cPoint[1]];
         var x, y;
         for (x = -1; x < 2; x++) {//一列一列的搜索
             for (y = -1; y < 2; y++) {
-                this.cnts++;
                 if (cPoint[0] + x >= 0 && cPoint[1] + y >= 0 && cPoint[0] + x < this.w && cPoint[1] + y < this.h) {
                     if (this.map[cPoint[0] + x][cPoint[1] + y] != 0) {//这个点不行走
                         this.dis[cPoint[0] + x][cPoint[1] + y] = -2;
@@ -95,12 +106,12 @@ class player {
             this.open.push(point);
         }
     }
-    getResult() {//路径已经找到，组合成一个数组
+    getResult(endPoint, startPoint) {//路径已经找到，组合成一个数组
         var result = new Array();
-        var $curLine = this.end;
+        var $curLine = endPoint;
         while (true) {
             result.unshift([this.disLast[$curLine[0]][$curLine[1]][0], this.disLast[$curLine[0]][$curLine[1]][1]]);
-            if ($curLine[0] == this.start[0] && $curLine[1] == this.start[1]) {
+            if ($curLine[0] == startPoint[0] && $curLine[1] == startPoint[1]) {
                 break;
             }
             $curLine = this.disLast[$curLine[0]][$curLine[1]];
@@ -113,25 +124,19 @@ class player {
             for (var $j = 0; $j < this.h; $j++) {
                 var color = this.map[$i][$j] ? "#FF0000" : "#FFFFFF";
                 this.drawBox($i * this.box, $j * this.box, this.box, color);
-                if (this.dis[$i][$j] > 0) {
-//                    this.drawText(this.dis[$i][$j], $i * this.box, $j * this.box + 8);
-                    this.drawBox($i * this.box, $j * this.box, this.box, "#FFFF00");
-                }
-//                this.drawText('(' + this.disLast[$i][$j][0] + ',' + this.disLast[$i][$j][1] + ')' + this.dis[$i][$j], $i * this.box, $j * this.box + 16);
             }
         }
-        var $curLine = this.end;
-        while (true) {
-            this.drawBox(this.disLast[$curLine[0]][$curLine[1]][0] * this.box, this.disLast[$curLine[0]][$curLine[1]][1] * this.box, this.box, "#00FF00");
-            if ($curLine[0] == this.start[0] && $curLine[1] == this.start[1]) {
-                break;
+    }
+    drawRouad() {
+        for (var x = 0; x < this.results.length - 1; x++) {
+            var curRoad = this.results[x];
+            for (var y = 0; y < curRoad.length; y++) {
+                this.drawBox(curRoad[y][0] * this.box, curRoad[y][1] * this.box, this.box, "#00FF00");
             }
-            $curLine = this.disLast[$curLine[0]][$curLine[1]];
+            this.drawBox(curRoad[0][0] * this.box, curRoad[0][1] * this.box, this.box, "#00F0F0");
+            this.drawText(x, curRoad[0][0] * this.box, curRoad[0][1] * this.box + 8);
         }
-        this.drawBox(this.start[0] * this.box, this.start[1] * this.box, this.box, "#0000FF");
-//        this.drawText('(' + this.start[0] + ',' + this.start[1] + ')' + this.dis[this.start[0]][this.start[1]], this.start[0] * this.box, this.start[1] * this.box + 8);
-        this.drawBox(this.end[0] * this.box, this.end[1] * this.box, this.box, "#00FFFF");
-//        this.drawText('(' + this.end[0] + ',' + this.end[1] + ')' + this.dis[this.end[0]][this.end[1]], this.end[0] * this.box, this.end[1] * this.box + 8);
+//        this.drawBox(this.results[x][y][0] * this.box, this.results[x][y][1] * this.box, this.box, "#00FFFF");
     }
     drawText(text, x, y) {
         this.canvas.font = '10px Arial';
